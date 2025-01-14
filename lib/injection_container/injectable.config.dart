aaecab43d06908978b0b1f34f5bfb4fde807d909
juliuszmandrosz <juliusz.mandrosz@gmail.com>
2025-01-14 17:08:03 +0100

    add firebase module

--- lib/injection_container/injectable.config.dart
+++ lib/injection_container/injectable.config.dart
@@ -8,12 +8,16 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:cloud_firestore/cloud_firestore.dart' as _i974;
+import 'package:cloud_functions/cloud_functions.dart' as _i809;
+import 'package:firebase_auth/firebase_auth.dart' as _i59;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 import 'package:loc_advisor/generate_recommendations/domain/recommendation_facade.dart'
     as _i877;
 import 'package:loc_advisor/generate_recommendations/infrastructure/firebase_recommendation_facade.dart'
     as _i318;
+import 'package:loc_advisor/modules/firebase_module.dart' as _i686;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -26,8 +30,16 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
+    final firebaseModule = _$FirebaseModule();
+    gh.lazySingleton<_i59.FirebaseAuth>(() => firebaseModule.firebaseAuth);
+    gh.lazySingleton<_i974.FirebaseFirestore>(
+        () => firebaseModule.firebaseFirestore);
+    gh.lazySingleton<_i809.FirebaseFunctions>(
+        () => firebaseModule.firebaseFunctions);
     gh.singleton<_i877.RecommendationFacade>(
         () => _i318.FirebaseRecommendationFacade());
     return this;
   }
 }
+
+class _$FirebaseModule extends _i686.FirebaseModule {}
---
+++ lib/modules/firebase_module.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
+
+@module
+abstract class FirebaseModule {
+  @lazySingleton
+  FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+
+  @lazySingleton
+  FirebaseFirestore get firebaseFirestore => FirebaseFirestore.instance;
+
+  @lazySingleton
+  FirebaseFunctions get firebaseFunctions => FirebaseFunctions.instance;
+}
